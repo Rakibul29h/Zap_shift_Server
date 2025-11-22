978fa31fccaaae9ae0e9d478be5477e379e76a61
Rakibul29h <rakibul2170@gmail.com>
2025-11-22 12:26:50 +0600

    mongodb connected

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors=require('cors');
+const app=express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const port=process.env.PORT || 3000;
+
+
+// MiddleWare use 
+
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e9cdfrv.mongodb.net/?appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+// connect mondoDb 
+async function run() {
+  try {
+    
+    await client.connect();
+    const db=client.db("Zap_Shift_DB");
+    const parcelCollection=db.collection("parcels");
+
+    // Send parcel Crud Operation
+    app.get("/parcels",async(req,res)=>{
+        const query={};
+        res.send("Kuttamara")
+    })
+
+    app.post('/parcels',async(req,res)=>{
+        const parcel=req.body;
+        const result=await parcelCollection.insertOne(parcel);
+        return res;
+    })
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   
+  }
+}
+run().catch(console.dir);
+// listener
+
+app.listen(port,()=>{
+    console.log(`Example app listening on port ${port}`);
+})
